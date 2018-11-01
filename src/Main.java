4b9670ee5d5cc654181de28dee5163a576572076
Nguyễn Đức Lộc <38336618+DucLoc1999@users.noreply.github.com>
2018-11-01 22:41:46 +0700

    commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+		MyFrame frame = new MyFrame();
+    frame.setVisible(true);
+    }
+}
+
+/**
+ * 
+ * --DictionaryManager.java:134)
+ *không chi lấy mỗi từ lít mà còn lấy từ khung
+ *
+ *--trong frame tool
+ *fix thêm speech to text
+ *
+ *--trong frame chính
+ *khi không tìm thấy mở bảng hỏi muốn translate không?
+ *  
+*/
